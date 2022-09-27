6b5ec496a5f626ff47ea65dcbbd1a16e52678b03
chintl510 <1145120614@qq.com>
2022-09-27 22:50:35 +0800

    完成了登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,32 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Router from 'vue-router'
+import Login from '@/components/Login.vue'
+import Home from '@/components/Home.vue'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
+const router = new Router({
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
+  ]
 })
+//为路由对象，添加beforEach导航守卫
+router.beforeEach((to, from, next) => {
+  //to将要访问的路径
+  //from代表从哪个路径跳转而来
+  //next是一个函数，表示放行
+  //next() 放行 next('/login') 强制跳转
+
+  //r如果页面访问login，直接放行
+  if (to.path === '/login') return next()
+  //从sessionStorage中获取token值
+  const tokenStr = window.sessionStorage.getItem('token')
+  //若没有token直接跳转到登录页
+  if (!tokenStr) return next('/login')
+  next()
+})
+
 
 export default router
